eed226b1ec63c714d3928824da7475911d00a4b6
Redor <matthieu.redor@hotmail.fr>
2018-06-12 14:51:16 +0200

    Initialisation du TP 'CodePropre'

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,25 @@
+package ex1;
+
+/**Class Calcul
+ * @author matth
+ */
+public class Calcul {
+
+	/** Fonction : Additionne les deux entiers en entrée
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int additionner(int a, int b){
+		return a+b;
+	}
+	
+	/** Fonction : Soustrait les deux entiers en entrée
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int soustraire(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,40 @@
+package ex1;
+
+import java.util.Date;
+
+/** Classe Entreprise
+ * @author matth
+ */
+public class Entreprise {
+
+	/** 
+	* siret : int 
+	*/
+	public int siret;
+	/** 
+	* nom : String 
+	*/
+	public String nom;
+	/** 
+	* adresse : String 
+	*/
+	public String adresse;
+	/** 
+	* dateCreation : Date 
+	*/
+	public Date dateCreation;
+	
+	/** 
+	* CAPITAL_MAX : int 
+	*/
+	public static final int CAPITAL_MAX = 3000000;
+	
+
+	/** Fonction :  Affiche la statut actuel de l'entreprise
+	 * 
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
